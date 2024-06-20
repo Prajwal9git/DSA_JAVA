8edfa0132caa44102b56e4f314bb9802cf615a8a
Prajwal9git <hprajwal200315@gmail.com>
2024-06-20 23:17:42 +0530

    made changes in example 2

--- src/example2.java
+++ src/example2.java
@@ -2,7 +2,7 @@ import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;gi
+import java.util.Scanner;
 
 public class example2 {
 
